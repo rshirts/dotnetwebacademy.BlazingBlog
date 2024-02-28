1c10f2a0c29360c44d88c6b2fc263c393100452f
Randee Shirts <rshirts@users.noreply.github.com>
2024-02-27 19:32:48 -0700

    Basic Service pattern Implementation of Application/Domain/Web layers

---
+++ BlazingBlog.Application/DepenencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using BlazingBlog.Application.Articles;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingBlog.Application {
+    public static class DepenencyInjection {
+        public static IServiceCollection AddApplication(this IServiceCollection services) {
+            services.AddScoped<IArticleService, ArticleService>();
+
+            return services;
+        }
+    }
+}
