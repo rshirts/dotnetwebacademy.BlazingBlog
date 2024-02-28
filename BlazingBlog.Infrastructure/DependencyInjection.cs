a55b3358ad462bdd454700d94c70493b4da89931
Randee Shirts <rshirts@users.noreply.github.com>
2024-02-28 00:38:38 -0700

    Added an abastract base entry

---
+++ BlazingBlog.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingBlog.Infrastructure {
+    public static class DependencyInjection {
+
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) {
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
